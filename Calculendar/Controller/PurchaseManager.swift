b3f986916e8cc77505c2be68361327ee9433b28d
Jay Hong <hjpyooo@gmail.com>
2023-12-25 14:53:37 +0900

    "In-App 구매 가져오기 실패메시지 추가" 및 Re-Indent

---
+++ Calculendar/Controller/PurchaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  PurchaseManager.swift
+//  Calculendar
+//
+//  Created by Jay on 1/14/24.
+//  Copyright © 2024 Jay. All rights reserved.
+//
+
+import Foundation
---
+++ Calculendar/Controller/SubscriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SubscriptionViewController.swift
+//  Calculendar
+//
+//  Created by Jay on 1/5/24.
+//  Copyright © 2024 Jay. All rights reserved.
+//
+
+import UIKit
+
+class SubscriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
